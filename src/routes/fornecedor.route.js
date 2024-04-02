387e8f1f97831d18f6eab7c998e448a9d84ab4d6
devDigitalMix <digitalmixdev@gmail.com>
2024-04-02 16:40:30 -0300

    fornecedor

--- src/routes/fornecedor.route.js
+++ src/routes/fornecedor.route.js
@@ -3,11 +3,14 @@ import fornecedorController from "../controllers/fornecedor.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const fornecedorRouter = Router();
-
-fornecedorRouter.use(authMiddleware);
+fornecedorRouter.get(
+  "/nome",
+  fornecedorController.findFornecedorByNameController
+);
 fornecedorRouter.get("/:id", fornecedorController.getFornecedorByIdController);
 fornecedorRouter.get("/", fornecedorController.findAllFornecedoresController);
 
+fornecedorRouter.use(authMiddleware);
 fornecedorRouter.post(
   "/create",
   fornecedorController.createFornecedorController
@@ -26,11 +29,6 @@ fornecedorRouter.get(
 fornecedorRouter.delete(
   "/delete/:id",
   fornecedorController.deleteFornecedorController
-);
-
-fornecedorRouter.get(
-  "/nome",
-  fornecedorController.findFornecedorByNameController
 );
 
 export default fornecedorRouter;
